9c413d21aff53a6ad32c9bc8bc7a28eed758bed4
syed umer <ssyedumer@gmail.com>
2023-12-31 23:50:00 +0500

    feat: countries module added

---
+++ modules/countries/controllers/countries.controllers.js
@@ -0,0 +1,75 @@
+
+const Response = require('../../../configurations/config.response'),
+    models = require('../../../configurations/config.sequelize').models,
+    { Op } = require('sequelize'),
+    globalUtils = require('../../utils');
+
+
+const fetchCountries = async (req, res, next) => {
+
+    try {
+
+        const limit = parseInt(req.query.limit) || 10,
+            offset = parseInt(req.query.offset) || 0,
+            sort = req.query.sort || 'DESC',
+            searchText = req.query.searchText || undefined;
+
+        let whereCluase = {};
+        if (req.query.searchText && req.query.searchText !== '') {
+
+            whereCluase = {
+
+                [Op.or]: [
+                    {
+                        name:{
+                            [Op.like]: `%${searchText}%`
+                        }
+                    },
+                    {
+                        region:{
+                            [Op.like]: `%${searchText}%`
+                        }
+                    },
+                    {
+                        countryCallingCode:{
+                            [Op.like]: `%${searchText}%`
+                        }
+                    }
+                ]
+            };
+
+        }
+        const data = await models.countries.findAndCountAll({
+            where: whereCluase,
+            attributes: ['id', 'name', 'countryCallingCode', 'flag', 'region'],
+            raw: true,
+            limit: limit,
+            offset: offset,
+            order: [
+                ['name', sort]
+            ]
+        });
+        return Response.sendResponse(
+            res,
+            {
+                msg: 1000,
+                data: {
+                    data
+
+                },
+                lang: req.params.lang
+            }
+        );
+
+    } catch (error) {
+
+        return next({ msg: 3067 });
+
+    }
+
+};
+
+
+module.exports = {
+    fetchCountries
+};
---
+++ modules/countries/routes/countries.routes.js
@@ -0,0 +1,18 @@
+const countryController = require('../controllers/countries.controllers'),
+    // authController = require('../controllers/auth.controllers'),
+    // userValidator = require('../validators/countries.validators'),
+    // userHelper = require('../controllers/helpers/users.helper'),
+    fileUpload = require('../../../configurations/config.multer'),
+    profileImage = fileUpload.upload(global.config.aws.s3.folders.profileImages),
+    passport = require('passport');
+
+module.exports = (app, version) => {
+
+    const moduleName = '/countries';
+    app.get(
+        `${version + moduleName}`,
+        countryController.fetchCountries
+
+    );
+
+};
