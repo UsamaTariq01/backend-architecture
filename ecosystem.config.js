9c940b8bfb49fda45511fe38da4c8c5e8add626d
umer <s.umer@moonlabor.com>
2024-01-01 17:08:58 +0500

    configuration: pm2 log rotate added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,19 @@ module.exports = {
             intances: 3,
             watch: true,
             watch_delay: 1000,
-            ignore_watch: [ 'node_modules' ],
+            ignore_watch: ['node_modules'],
             exp_backoff_restart_delay: 100,
             env: {
                 NODE_ENV: 'development',
                 PORT: 3000
-            }
+            },
+            // Log configuration
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            out_file: './logs/out.log',
+            error_file: './logs/error.log',
+            logRotate: true, // Enable log rotation
+            max_size: '10M', // Max size of the log file before rotation
+            retain: 5 // Number of rotated log files to keep
         }
     ]
 };
